5ac321a01831c8543995de32024f56d008d67fad
Tol <htemohpab@gmail.com>
2022-05-08 18:08:11 +0600

    data(repository implementation)

--- app/src/main/java/com/abdykadyr/mathgame/presentation/MainActivity.kt
+++ app/src/main/java/com/abdykadyr/mathgame/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.abdykadyr.mathgame
+package com.abdykadyr.mathgame.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.abdykadyr.mathgame.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
